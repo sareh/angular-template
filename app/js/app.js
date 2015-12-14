10ffe06668d00e48a6f2dc9ac1a55378a1b1bc8f
sareh <sareh.heidari@gmail.com>
2015-12-14 11:28:01 +0000

    New files for app, index & scss

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict';
+
+  var dependencies = [
+    'ngAria'
+    'ui.router',
+    'angular-jwt'
+  ];
+
+  module.exports = angular.module('templateApp', dependencies);
+})();
