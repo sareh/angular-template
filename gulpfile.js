acb9d213c57226aa7ebc9a21709a5ac573a9d663
sareh <sareh.heidari@gmail.com>
2015-12-14 14:17:46 +0000

    Rearranged file structure

--- gulpfile.js
+++ gulpfile.js
@@ -35,4 +35,16 @@ gulp.task('uglifycss', function() {
     .pipe(gulp.dest('dist'))
 })
 
+gulp.task('autoprefixer', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./stylesheets/css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./dest'));
+});
+
 gulp.task('default', ['styles', 'watch']);
