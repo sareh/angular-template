717181ae0641bf4bb8f36251575d2b8265c41512
sareh <sareh.heidari@gmail.com>
2015-12-17 00:44:21 +0000

    Fixing bower/npm/gulp/foundation scss order

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,51 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('./package.json'),
+    sass_globbing: {
+      your_target: {
+        options: {
+          useSingleQuotes: false,
+          signature: '// Hello, World!'
+        },
+        files: {
+          './stylesheets/scss/foundation-imports.scss': './bower_components/foundation-sites/foundation-sites-6.0.6/scss/**/*.scss',
+          // './stylesheets/scss/imports.scss': './stylesheets/scss/app.scss',
+        }
+      }
+    },
+    sass_2: {
+      options: {
+        sourceMap: true
+      },
+      develop: {
+        files: {
+          'main.css': 'imports.scss'
+        }
+      }
+    },
     sass: {
       dist: {
         files: {
+          // './dist/css/foundation.css': [
+          //   // './bower_components/foundation-sites/scss/foundation.scss',
+          //   // './bower_components/foundation-sites/scss/settings/_settings.scss',
+          //   // './bower_components/foundation-sites/scss/**/*.scss'
+
+          //   './bower_components/foundation-sites/scss/_global.scss'
+          //   ],
           './dist/css/app.css': './stylesheets/scss/app.scss'
         }
       }
     },
-   watch: {
-    css: {
-      files: './stylesheets/scss/**/*.scss',
-      tasks: ['sass']
+    watch: {
+      css: {
+        files: './stylesheets/scss/**/*.scss',
+        tasks: ['sass']
+      }
     }
-  }
-});
+  });
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default',['watch']);
+  grunt.loadNpmTasks('grunt-sass-globbing');
+  grunt.registerTask('default',['sass']);
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,146 @@
-var gulp         = require('gulp');
-// var jshint       = require('gulp-jshint')
-var sass         = require('gulp-sass');
-var susy         = require('susy');
-// var watch        = require('gulp-watch');
-var rename       = require('gulp-rename');
-var concat       = require('gulp-concat');
-var replace      = require('gulp-replace');
-var livereload   = require('gulp-livereload');
-// var minify       = require('gulp-minify');
-// var uglify       = require('gulp-uglify');
-var uglifycss    = require('gulp-uglifycss');
-var postcss      = require('gulp-postcss');
-var sourcemaps   = require('gulp-sourcemaps');
-var autoprefixer = require('autoprefixer');
-// var concat-vendor = require('gulp-concat-vendor');
-// var gzip         = require('gulp-gzip');
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
 
-
-var gzip_options = {
-  threshold: '1kb',
-  gzipOptions: {
-    level: 9
-  }
-};
-
-var cssDest = './dist/css/';
-var jsDest  = './dist/js/';
-var scssDir = './stylesheets/scss/';
-
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch(scssDir+'app.scss',['sass-css'], function(){
-    livereload.reload('index.html');
-  });
-});
-
-gulp.task('sass-css', function() {
-  gulp.src(scssDir+'app.scss')
-      .pipe(sourcemaps.init())
-      .pipe(sass().on('error', sass.logError))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest(cssDest))
-      .pipe(livereload());
-});
-
-gulp.task('default', ['sass-css', 'watch']);
-
-gulp.task('styles', function() {
-  gulp.src(scssDir+'app.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest(cssDest))
-      .pipe(livereload());
-});
-
-gulp.task('sass-css-min-gzip', function() {
-  return gulp.src(scssDir+'*.scss')
-            .pipe(sass().on('error', sass.logError))
-            .pipe(gulp.dest(cssDest))
-            .pipe(rename({suffix: '.min'}))
-            .pipe(uglifycss())
-            .pipe(gulp.dest(cssDest))
-            // .pipe(gzip(gzipOptions))
-            // .pipe(gulp.dest(cssDest))
-            .pipe(livereload());
-});
-
-// gulp.task('scripts', function() {
-//   gulp.src('./vendor/*')
-//   .pipe(vendor('vendor.js'))
-//   .pipe(gulp.dest('./dist/'));  
-// });
-
-
-// http://foundation.zurb.com/sites/docs/v/5.5.3/sass.html
-gulp.task('new-sass',function() {
-  gulp.src(scssDir+'*.scss')
-    .pipe(sass({
-      includePaths: ['bower_components/foundation-sites/scss']
-    }))
-    .pipe(gulp.dest(cssDest));
-})
+var scssPath  = './stylesheets/scss/';
+var cssPath   = './dist/css/';
+var jsPath    = './dist/js/';
+var sassPaths = [
+  './bower_components/foundation-sites/scss',
+  './bower_components/motion-ui/src'
+];
 
 gulp.task('sass', function() {
-  gulp.src(scssDir+'app.scss')
-    .pipe(sass({
-      loadPath: ['bower_components/foundation-apps/scss', 'stylesheets/scss'],
-      style: 'nested',
-      bundleExec: true
-    }))
-    .on('error', function(e) {
-      console.log(e);
+  return gulp.src(scssPath+'app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths
     })
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions', 'ie 10']
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest(cssDest))
-    .pipe(connect.reload());
+    .pipe(gulp.dest(cssPath));
 });
 
-gulp.task('uglifycss', function() {
-  gulp.src(cssDest+'*.css')
-    .pipe(uglifycss({
-      "max-line-len": 80
-    }))
-    .pipe(gulp.dest('cssDest'))
-})
-
-gulp.task('autoprefixer', function () {
-  gulp.src(cssDest+'*.css')
-    .pipe(sourcemaps.init())
-    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(cssDest));
+gulp.task('default', ['sass'], function() {
+  gulp.watch([scssPath+'**/*.scss'], ['sass']);
 });
+
+
+
+// var gulp         = require('gulp');
+// // var jshint       = require('gulp-jshint')
+// var sass         = require('gulp-sass');
+// var susy         = require('susy');
+// // var watch        = require('gulp-watch');
+// var rename       = require('gulp-rename');
+// var concat       = require('gulp-concat');
+// var replace      = require('gulp-replace');
+// var livereload   = require('gulp-livereload');
+// // var minify       = require('gulp-minify');
+// // var uglify       = require('gulp-uglify');
+// var uglifycss    = require('gulp-uglifycss');
+// var postcss      = require('gulp-postcss');
+// var sourcemaps   = require('gulp-sourcemaps');
+// var autoprefixer = require('autoprefixer');
+// // var concat-vendor = require('gulp-concat-vendor');
+// // var gzip         = require('gulp-gzip');
+
+
+// var gzip_options = {
+//   threshold: '1kb',
+//   gzipOptions: {
+//     level: 9
+//   }
+// };
+
+// var cssDest = './dist/css/';
+// var jsDest  = './dist/js/';
+// var scssDir = './stylesheets/scss/';
+
+// gulp.task('watch', function() {
+//   livereload.listen();
+//   gulp.watch(scssDir+'app.scss',['sass-css'], function(){
+//     livereload.reload('index.html');
+//   });
+// });
+
+// gulp.task('sass-css', function() {
+//   gulp.src(scssDir+'app.scss')
+//       .pipe(sourcemaps.init())
+//       .pipe(sass().on('error', sass.logError))
+//       .pipe(sourcemaps.write())
+//       .pipe(gulp.dest(cssDest))
+//       .pipe(livereload());
+// });
+
+// gulp.task('default', ['sass-css', 'watch']);
+
+// gulp.task('styles', function() {
+//   gulp.src(scssDir+'app.scss')
+//       .pipe(sass().on('error', sass.logError))
+//       .pipe(gulp.dest(cssDest))
+//       .pipe(livereload());
+// });
+
+// gulp.task('sass-css-min-gzip', function() {
+//   return gulp.src(scssDir+'*.scss')
+//             .pipe(sass().on('error', sass.logError))
+//             .pipe(gulp.dest(cssDest))
+//             .pipe(rename({suffix: '.min'}))
+//             .pipe(uglifycss())
+//             .pipe(gulp.dest(cssDest))
+//             // .pipe(gzip(gzipOptions))
+//             // .pipe(gulp.dest(cssDest))
+//             .pipe(livereload());
+// });
+
+// // gulp.task('scripts', function() {
+// //   gulp.src('./vendor/*')
+// //   .pipe(vendor('vendor.js'))
+// //   .pipe(gulp.dest('./dist/'));  
+// // });
+
+
+// // http://foundation.zurb.com/sites/docs/v/5.5.3/sass.html
+// gulp.task('new-sass',function() {
+//   gulp.src(scssDir+'*.scss')
+//     .pipe(sass({
+//       includePaths: ['bower_components/foundation-sites/scss']
+//     }))
+//     .pipe(gulp.dest(cssDest));
+// })
+
+// gulp.task('sass', function() {
+//   gulp.src(scssDir+'app.scss')
+//     .pipe(sass({
+//       loadPath: ['bower_components/foundation-apps/scss', 'stylesheets/scss'],
+//       style: 'nested',
+//       bundleExec: true
+//     }))
+//     .on('error', function(e) {
+//       console.log(e);
+//     })
+//     .pipe(autoprefixer({
+//       browsers: ['last 2 versions', 'ie 10']
+//     }))
+//     .pipe(gulp.dest(cssDest))
+//     .pipe(connect.reload());
+// });
+
+// gulp.task('uglifycss', function() {
+//   gulp.src(cssDest+'*.css')
+//     .pipe(uglifycss({
+//       "max-line-len": 80
+//     }))
+//     .pipe(gulp.dest('cssDest'))
+// })
+
+// gulp.task('autoprefixer', function () {
+//   gulp.src(cssDest+'*.css')
+//     .pipe(sourcemaps.init())
+//     .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest(cssDest));
+// });
 
 
